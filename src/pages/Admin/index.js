25b192bce5c81818b38247376c99029230af0d71
alexandrtovmach <alexandrtovmach@gmail.com>
2019-09-26 16:11:25 +0300

    patch: Initialized file structure

---
+++ src/pages/Admin/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+class Admin extends React.Component {
+  componentDidMount() {
+    // code here
+
+    console.error("Skill tree for one month... it's error!");
+  }
+
+  render() {
+    return <div>Admin Page</div>;
+  }
+}
+
+Admin.propTypes = {};
+Admin.defaultProps = {};
+
+export default Admin;
---
+++ src/pages/Login/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+class Login extends React.Component {
+  componentDidMount() {
+    // code here
+
+    console.error("Skill tree for one month... it's error!");
+  }
+
+  render() {
+    return <div>Login Page</div>;
+  }
+}
+
+Login.propTypes = {};
+Login.defaultProps = {};
+
+export default Login;
